71feef79573f701227a83eaa02a218d3c398d438
RehanScript <r.h.mediahouse.official@gmail.com>
2026-04-12 23:33:32 +0530

    Inline stylesheets explicitly to eliminate render blocking

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zynfexgame.me',
+  build: {
+    inlineStylesheets: 'always'
+  },
   vite: {
     plugins: [tailwindcss()]
   },
